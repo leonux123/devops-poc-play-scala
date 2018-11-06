6b2a975ea2c08b02cc5116f1ed204a917952f402
leonux123 <arias11mx@gmail.com>
2018-11-06 12:17:37 -0600

    Test 4

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -19,7 +19,7 @@ class HomeController @Inject()(cc: ControllerComponents) (implicit assetsFinder:
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("ESS POC v67.1 - master"))
+    Ok(views.html.index("ESS POC v69.1 - master"))
   }
 
 }
